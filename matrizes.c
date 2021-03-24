5c0ad2222b7b23180eb5dc4ac987d3ae7db75427
dbs-97 <danielbrito4297@gmail.com>
2021-03-24 19:26:41 -0300

    update

--- matrizes.c
+++ matrizes.c
@@ -3,49 +3,51 @@
 #include <math.h>
 
 struct Matriz {
-	float* elementos;
-	int n; //Dimensão da matriz quadrada
+        float* elementos;
+        int n; //Dimensão da matriz quadrada
 };
 
 typedef struct Matriz Matriz;
 
-Matriz* init(int n){
-	Matriz *A = (Matriz *) malloc(sizeof(A) * pow(A->n,2));
-	if(A != NULL){
-		printf("Até aqui ok");
-		(*A).elementos = NULL;
-		(*A).n = n;
-		return A;
-	};
-	printf("É já deu BO no init");
-	return NULL;
+Matriz* init(int n){ 
+        Matriz *A = (Matriz *) malloc(sizeof(Matriz) * (unsigned int) pow(A->n,2));
+        if(A != NULL){
+                printf("Até aqui ok");
+                A->elementos = NULL;
+                A->n = n;
+                return A;
+        };
+        printf("É já deu BO no init");
+        return NULL;
 };
 
 void fill_rand(Matriz *A){
-	for(int i = 0; i < pow(((*A).n),2); i++){
-		(*A).elementos[i] = rand();
-	};
+        printf("Mic check mic check");
+        for(int i = 0; i < pow(((*A).n),2); i++){
+                (*A).elementos[i] = rand();
+        };
 };
 
 void print(Matriz *A){
-	for(int i=0; i < pow(((*A).n), 2); i++){
-		printf("%f ", (*A).elementos[i]);
-		if(fmod(A->elementos[i],(float)A->n) == 0){
-			printf("/n");
-		};
-	};
+        for(int i=0; i < pow(((*A).n), 2); i++){
+                printf("%f ", (*A).elementos[i]);
+                if(fmod(A->elementos[i],(float)A->n) == 0){ 
+                        printf("/n");
+                };
+        };
 };
+
 
 void main(){
-	printf("Hello \n");
-	Matriz *A = init(2);
-	fill_rand(A);
-	print(A);
+        printf("%zu \n", sizeof(Matriz));
+	printf("%zu \n", sizeof(Matriz)* (int)pow(3,2));
+        printf("Hello alguma coisa ");
+        printf("Teste");
+       Matriz *A = init(2);
+	printf("TesteB");
+        //fill_rand(A);
+	printf("testeC");
+//        print(A);
 };
-	
-
-
-//Tentar depois cirar uma função que percorre a matriz, 
-// e recebe outra função pra aplicar em cada elemento (criar um map né)
-
-
+   
+//sizeof(Matriz) * (int) pow(A->n,2))
